b72dd52b447b1c3437ce4d14874576f5e1618b8b
Sean Tegtmeyer <sean.tegtmeyer@gmail.com>
2022-11-23 00:07:42 -0800

    Update flip.py
    Added support for running X flips Y times, and printing stats among all runs for max diff between heads / tails, longest string of heads / tails in a row.

--- flip.py
+++ flip.py
@@ -1,25 +1,34 @@
 import random, os, time, datetime, argparse
 
-heads = 0
-tails = 0
-flipped = 0
+numHeads = 0
+numTails = 0
 i = 0
+j = 0
 tempTailValue = 0
 tempHeadValue = 0
-longTailValue = 0
-longHeadValue = 0
+mostTailsConsecutive = 0
+mostHeadsConsecutive = 0
+tailsPercent = 0
+headsPercent = 0
+difference = 0
+statsList = []
 
 parser = argparse.ArgumentParser(description="Simulate a coin flip a certain amount of times and display the results.")
 parser.add_argument('-n', nargs=1)
 parser.add_argument('-l', nargs=1)
 parser.add_argument('-o', nargs=1)
+parser.add_argument('-nr', nargs=1)
 args = parser.parse_args()
 
 print("The following is a program that simulates a coin flip a certain amount of times, and displays the results.")
 
-def askHowManyTimesToRepeat():
-    timesToRepeat = int(input("Enter the amount of times you would like the coin to be flipped: "))
-    return timesToRepeat
+def askHowManyTimesToFlipCoin():
+    timesToFlipCoin = int(input("Enter the amount of times you would like the coin to be flipped: "))
+    return timesToFlipCoin
+
+def askHowManyTimesToRunFlipCoin():
+    timesToRunFlipCoin= int(input("Enter the amount of times you would like to run flip coin: "))
+    return timesToRunFlipCoin
 
 def askToLogOrNot():
     logFileYesOrNo = input("Would you like to save a logfile? Y/N: ")
@@ -34,61 +43,84 @@ def askToShowVisualOutput():
     return visualOutputYesOrNo
 
 def flip(times):
-	global flipped, tails, heads, tempTailValue, tempHeadValue, longTailValue, longHeadValue
+	global numTails, numHeads, tempTailValue, tempHeadValue, mostTailsConsecutive, mostHeadsConsecutive
+
 	for i in range(times):
 		flip = random.randrange(2)
-		flipped += 1
 		if flip == 0:
-			heads = heads+1
+			numHeads = numHeads+1
 			tempHeadValue = tempHeadValue+1
-			if longTailValue < tempTailValue:
-				longTailValue = tempTailValue
+			if mostTailsConsecutive < tempTailValue:
+				mostTailsConsecutive = tempTailValue
 				tempTailValue = 0
 			else:
 				tempTailValue = 0
 		else:
-			tails = tails+1
+			numTails = numTails+1
 			tempTailValue = tempTailValue+1
-			if longHeadValue < tempHeadValue:
-				longHeadValue = tempHeadValue
+			if mostHeadsConsecutive < tempHeadValue:
+				mostHeadsConsecutive = tempHeadValue
 				tempHeadValue = 0
 			else:
 				tempHeadValue = 0
 		if visualOutputYesOrNo == "y":
 			print(flip, end=" ")
 
-def outputAnalysis():
-	tailspercent = (100/timesToRepeat)*tails
-	headspercent = (100/timesToRepeat)*heads
-	if tails > heads:
-		difference = tails - heads
-	elif heads > tails:
-	    difference = heads - tails
+	if mostTailsConsecutive < tempTailValue:
+		mostTailsConsecutive = tempTailValue
+	if mostHeadsConsecutive < tempHeadValue:
+		mostHeadsConsecutive = tempHeadValue
+
+def calculatePercentagesAndDifference():
+	global tailsPercent, headsPercent, difference
+
+	tailsPercent = (100/timesToFlipCoin)*numTails
+	headsPercent = (100/timesToFlipCoin)*numHeads
+	if numTails > numHeads:
+		difference = numTails - numHeads
+	elif numHeads > numTails:
+	    difference = numHeads - numTails
 	else:
 	    difference = 0
 
+"""
 	elapsedtime = str(datetime.timedelta(seconds=round(time.time()-starttime, 2)))
 	elapsedtime = elapsedtime[:-4]
+"""
+def outputAnalysis():
+	mostHeadsConsecutiveAllRuns = 0
+	mostTailsConsecutiveAllRuns = 0
+	maxDifference = 0
+	maxDiffNumHeads = 0
+	maxDiffNumTails = 0
+
+	for x in statsList:
+		if x["mostHeadsConsecutive"] > mostHeadsConsecutiveAllRuns:
+			mostHeadsConsecutiveAllRuns = x["mostHeadsConsecutive"]
+		if x["mostTailsConsecutive"] > mostTailsConsecutiveAllRuns:
+			mostTailsConsecutiveAllRuns = x["mostTailsConsecutive"]
+		if x["difference"] > maxDifference:
+			maxDifference = x["difference"]
+			maxDiffNumHeads = x["numHeads"]
+			maxDiffNumTails = x["numTails"]
 
 	print("\n\n")
-	print("Flipped a coin", timesToRepeat, "times.")
-	print("Total execution time:", elapsedtime)
-	print("Tails was flipped", tails, "times, or", tailspercent, "% of the time.")
-	print("Heads was flipped", heads, "times, or", headspercent, "% of the time.")
-	print("The difference between the two was", difference, ".")
-	print("The longest string of tails in a row was:", longTailValue)
-	print("The longest string of heads in a row was:", longHeadValue)
+	print("---------** ALL Runs Stats **---------")
+	print("The max difference between the heads and tails was:", maxDifference, "heads[", maxDiffNumHeads, "]", "tails[", maxDiffNumTails, "]")
+	print("The longest string of tails in a row was:", mostTailsConsecutiveAllRuns)
+	print("The longest string of heads in a row was:", mostHeadsConsecutiveAllRuns)
+
 
 def outputLogFile():
 	if logFileName.lower() != "n":
 		logfile = open(os.getcwd()+'/'+logFileName, 'w')
-		logfile.write("Flipped a coin " + str(timesToRepeat) + " times.\n")
+		logfile.write("Flipped a coin " + str(timesToFlipCoin) + " times.\n")
 		logfile.write("Total execution time: " + str(elapsedtime) + "\n")
-		logfile.write("Tails was flipped " + str(tails) + " times, or " + str(tailspercent) + "% of the time.\n")
-		logfile.write("Heads was flipped " + str(heads) + " times, or " + str(headspercent) + "% of the time.\n")
+		logfile.write("Tails was flipped " + str(numTails) + " times, or " + str(tailsPercent) + "% of the time.\n")
+		logfile.write("Heads was flipped " + str(numHeads) + " times, or " + str(headsPercent) + "% of the time.\n")
 		logfile.write("The difference between the two was " + str(difference) + ".\n")
-		logfile.write("The longest string of tails in a row was " + str(longTailValue) + ".\n")
-		logfile.write("The longest string of heads in a row was " + str(longHeadValue) + ".\n")
+		logfile.write("The longest string of tails in a row was " + str(mostTailsConsecutive) + ".\n")
+		logfile.write("The longest string of heads in a row was " + str(mostHeadsConsecutive) + ".\n")
 		logfile.write("The amount of time taken to flip each coin on average was " + str(estimate) + ".\n")
 		logfile.close()
 		print("A logfile was published to ", os.getcwd()+'/'+logFileName)
@@ -96,9 +128,14 @@ def outputLogFile():
 		print("No log created.")
 
 if args.n == None:
-    timesToRepeat = askHowManyTimesToRepeat()
+    timesToFlipCoin = askHowManyTimesToFlipCoin()
 else:
-    timesToRepeat = int(args.n[0])
+    timesToFlipCoin = int(args.n[0])
+
+if args.nr == None:
+    timesToRunFlipCoin = askHowManyTimesToRunFlipCoin()
+else:
+    timesToRunFlipCoin = int(args.nr[0])
 
 if args.l == None:
     logFileName = askToLogOrNot()
@@ -113,6 +150,30 @@ else:
     visualOutputYesOrNo = args.o[0].lower()
 
 starttime = time.time()
-flip(timesToRepeat)
+for j in range(timesToRunFlipCoin):
+	flip(timesToFlipCoin)
+	calculatePercentagesAndDifference()
+	statsList.append({'numHeads': numHeads, 'numTails': numTails, 'mostHeadsConsecutive': mostHeadsConsecutive, 'mostTailsConsecutive': mostTailsConsecutive, 'tailsPercent': tailsPercent, 'headsPercent': headsPercent, 'difference': difference})
+
+	i = 0
+	numHeads = 0
+	numTails = 0
+	tempTailValue = 0
+	tempHeadValue = 0
+	mostTailsConsecutive = 0
+	mostHeadsConsecutive = 0
+	tailsPercent = 0
+	headsPercent = 0
+	difference = 0
+
+j = 0
+for j in range(timesToRunFlipCoin):
+	print("\n\n")
+	print("Tails was flipped", statsList[j]['numTails'], "times, or", statsList[j]['tailsPercent'], "% of the time.")
+	print("Heads was flipped", statsList[j]['numHeads'], "times, or", statsList[j]['headsPercent'], "% of the time.")
+	print("The difference between the two was", statsList[j]['difference'], ".")
+	print("The longest string of tails in a row was:", statsList[j]['mostTailsConsecutive'])
+	print("The longest string of heads in a row was:", statsList[j]['mostHeadsConsecutive'])
+
 outputAnalysis()
 outputLogFile()
