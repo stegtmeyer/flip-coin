d04020ae2a354ee3356acc1a0e6ad6da11f3d473
Sean Tegtmeyer <sean.tegtmeyer@gmail.com>
2022-11-26 02:31:22 -0800

    Revert "Added support for testing a specific betting strategy"
    This reverts commit 0b07c5df1474c2b442a14af5705d70d904d8dd7b.

--- flip.py
+++ flip.py
@@ -8,92 +8,40 @@ def askHowManyTimesToRunFlipCoin():
     timesToRunFlipCoin= int(input("Enter the number of times you would like to run n flips: "))
     return timesToRunFlipCoin
 
-def setBetParameters():
-	doubleAfterFirstNConsecutiveLosses = (False, False, False, False, False, False, False, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False)
-	halveAfterBreakEven = 1
-	betParameters = {'minBetSize': 10, 'doubleAfterFirstNConsecutiveLosses': doubleAfterFirstNConsecutiveLosses, 'halveAfterBreakEven': 1}
-	return betParameters
-
-def flip(timesToFlipCoin, betParameters):
+def flip(timesToFlipCoin):
 	i = 0
 	numHeads = 0
 	numTails = 0
-	numHeadsConsecutive = 0
-	numTailsConsecutive = 0
 	mostHeadsConsecutive = 0
 	mostTailsConsecutive = 0
-	moneyWon = 0
-	numFlipsToBreakeven = 0
-	numFlipsToHalving = 0
-	amountToBreakeven = 0
-	amountForNextHalving = 0
-	tryingToBreakeven = False
-	currentBetSize = betParameters['minBetSize']
+	tempTailValue = 0
+	tempHeadValue = 0
 	
 	for i in range(timesToFlipCoin):
 		flip = random.randrange(2)
 		if flip == 0:
-			numHeads += 1
-			numHeadsConsecutive += 1
-			# Assume heads is a win, tails is a loss
-			moneyWon += currentBetSize
-			if (tryingToBreakeven):
-				numFlipsToBreakeven += 1
-				numFlipsToHalving += 1
-				amountToBreakeven -= currentBetSize
-				if (amountToBreakeven <= 0):
-					tryingToBreakeven = False
-					print("Broke even after [", numFlipsToBreakeven, "] flips")
-					numFlipsToBreakeven = 0
-					amountToBreakeven = 0
-					amountForNextHalving = 0
-					numFlipsToHalving = 0
-					currentBetSize = betParameters['minBetSize']
-				elif (amountToBreakeven <= amountForNextHalving):
-						currentBetSize /= 2
-						if (currentBetSize == betParameters['minBetSize']):
-							amountForNextHalving = 0
-						else:
-							# hack constant for numConsecutiveLosses before doubling bet size. Will remove as part of major refactor splitting flipping from betting strategy
-							amountForNextHalving = amountToBreakeven - 7 * currentBetSize
-						print("Halved bet after [", numFlipsToHalving, "] flips", "amountToBreakeven [", amountToBreakeven, "] amountForNextHalving [", amountForNextHalving, "]")
-						numFlipsToHalving = 0
-			if mostTailsConsecutive < numTailsConsecutive:
-				mostTailsConsecutive = numTailsConsecutive
-				numTailsConsecutive = 0
+			numHeads = numHeads+1
+			tempHeadValue = tempHeadValue+1
+			if mostTailsConsecutive < tempTailValue:
+				mostTailsConsecutive = tempTailValue
+				tempTailValue = 0
 			else:
-				numTailsConsecutive = 0
+				tempTailValue = 0
 		else:
-			numTails += 1
-			numTailsConsecutive += 1
-			# Assume heads is a win, tails is a loss
-			moneyWon -= currentBetSize
-			if (tryingToBreakeven):
-				numFlipsToBreakeven += 1
-				amountToBreakeven += currentBetSize
-
-			if len(betParameters["doubleAfterFirstNConsecutiveLosses"]) > numTailsConsecutive:
-				if betParameters["doubleAfterFirstNConsecutiveLosses"][numTailsConsecutive] == True:
-					tryingToBreakeven = True
-					amountToBreakeven = amountToBreakeven + numTailsConsecutive * currentBetSize
-					amountForNextHalving = amountToBreakeven - numTailsConsecutive * currentBetSize
-					currentBetSize *= 2
-					print("Due to [", numTailsConsecutive, "] consecutive losses, doubling bet size to [", currentBetSize, "] in attempt to break even amountToBreakeven [$", amountToBreakeven, "] ", "amountForNextHalving [", amountForNextHalving, "]")
+			numTails = numTails+1
+			tempTailValue = tempTailValue+1
+			if mostHeadsConsecutive < tempHeadValue:
+				mostHeadsConsecutive = tempHeadValue
+				tempHeadValue = 0
 			else:
-				print("No instructions for whether to double bet, as doubleAfterFirstNConsecutiveLosses tupple length is shorter than numTailsConsecutive [", numTailsConsecutive, "]")
-			if mostHeadsConsecutive < numHeadsConsecutive:
-				mostHeadsConsecutive = numHeadsConsecutive
-				numHeadsConsecutive = 0
-			else:
-				numHeadsConsecutive = 0
+				tempHeadValue = 0
 
-	# Check for consecutive to account for not going through flip loop again
-	if mostTailsConsecutive < numTailsConsecutive:
-		mostTailsConsecutive = numTailsConsecutive
-	if mostHeadsConsecutive < numHeadsConsecutive:
-		mostHeadsConsecutive = numHeadsConsecutive
+	if mostTailsConsecutive < tempTailValue:
+		mostTailsConsecutive = tempTailValue
+	if mostHeadsConsecutive < tempHeadValue:
+		mostHeadsConsecutive = tempHeadValue
 	
-	return numHeads, numTails, mostHeadsConsecutive, mostTailsConsecutive, moneyWon
+	return numHeads, numTails, mostHeadsConsecutive, mostTailsConsecutive
 
 def calculatePercentagesAndDifference():
 	tailsPercent = (100/timesToFlipCoin)*numTails
@@ -151,10 +99,9 @@ else:
 statsList = []
 starttime = time.time()
 for j in range(timesToRunFlipCoin):
-	betParameters = setBetParameters()
-	numHeads, numTails, mostHeadsConsecutive, mostTailsConsecutive, moneyWon = flip(timesToFlipCoin, betParameters)
+	numHeads, numTails, mostHeadsConsecutive, mostTailsConsecutive = flip(timesToFlipCoin)
 	tailsPercent, headsPercent, difference = calculatePercentagesAndDifference()
-	statsList.append({'numHeads': numHeads, 'numTails': numTails, 'mostHeadsConsecutive': mostHeadsConsecutive, 'mostTailsConsecutive': mostTailsConsecutive, 'tailsPercent': tailsPercent, 'headsPercent': headsPercent, 'difference': difference, 'moneyWon': moneyWon})
+	statsList.append({'numHeads': numHeads, 'numTails': numTails, 'mostHeadsConsecutive': mostHeadsConsecutive, 'mostTailsConsecutive': mostTailsConsecutive, 'tailsPercent': tailsPercent, 'headsPercent': headsPercent, 'difference': difference})
 	
 	print("\n\n")
 	print("Tails was flipped", statsList[j]['numTails'], "times, or", statsList[j]['tailsPercent'], "% of the time.")
@@ -162,6 +109,5 @@ for j in range(timesToRunFlipCoin):
 	print("The difference between the two was", statsList[j]['difference'], ".")
 	print("The longest string of tails in a row was:", statsList[j]['mostTailsConsecutive'])
 	print("The longest string of heads in a row was:", statsList[j]['mostHeadsConsecutive'])
-	print("The amount of money won was: $", statsList[j]['moneyWon'])
 
 outputAnalysis(statsList)
